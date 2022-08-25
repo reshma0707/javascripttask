b5f8ac2ab3a64980d8166eb0809fc722f60db828
reshma0707 <110984997+reshma0707@users.noreply.github.com>
2022-08-25 14:57:18 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
 
 //arithmatic operation 
 
-var num1: number = 2
-var num2: number = 3
+//arithmatic operation 
 
-console.log("sub :" +(num1-num2))
-console.log("add :" +(num1+num2))
-console.log("mul :" +(num1*num2))
-console.log("div :" +(num1/num2))
+function add(num1:number,num2:number){
+    console.log("add:"+(num1+num2))
+}
+add(2,3)
+
+
+function sub(num1:number,num2:number){
+    console.log("sub:"+(num1-num2))
+}
+sub(6,3)
+
+function mul(num1:number,num2:number){
+    console.log("mul:"+(num1*num2))
+}
+mul(6,3)
+
+function div(num1:number,num2:number){
+    console.log("div:"+(num1-num2))
+}
+div(6,3)
