1058c43d42af6354d232da2bd04a449bcb91a3d1
reshma0707 <110984997+reshma0707@users.noreply.github.com>
2022-08-25 11:36:25 +0530

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,40 @@
+export{}
+let message = "hello world"
+console.log(message)
+
+
+let x : number = 23;
+console.log(x)
+
+
+let name : string = "resh"
+console.log(name)
+
+
+let reshma : string = `hey you ${name} how are you`
+console.log(reshma)
+
+//arithmatic operation 
+
+let num1: number = 2
+let num2: number = 3
+let add: number  =0;
+add = num1 + num2 ;
+console.log(add)
+
+let num3: number = 2
+let num4: number = 3
+let sub: number  =0;
+sub = num3 - num4 ;
+console.log(sub)
+
+let num5: number = 2
+let num6: number = 3
+let mul: number  =0;
+mul = num3 * num4 ;
+console.log(mul)
+
+console.log(typeof(reshma))
+console.log(typeof(num1))
+console.log(typeof(num2))
+
