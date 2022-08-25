caaa8c15b11038f322d8c0fd5d5e2da512a57c69
reshma0707 <110984997+reshma0707@users.noreply.github.com>
2022-08-25 14:01:08 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,40 +1,10 @@
-export{}
-let message = "hello world"
-console.log(message)
-
-
-let x : number = 23;
-console.log(x)
-
-
-let name : string = "resh"
-console.log(name)
-
-
-let reshma : string = `hey you ${name} how are you`
-console.log(reshma)
 
 //arithmatic operation 
 
-let num1: number = 2
-let num2: number = 3
-let add: number  =0;
-add = num1 + num2 ;
-console.log(add)
+var num1: number = 2
+var num2: number = 3
 
-let num3: number = 2
-let num4: number = 3
-let sub: number  =0;
-sub = num3 - num4 ;
-console.log(sub)
-
-let num5: number = 2
-let num6: number = 3
-let mul: number  =0;
-mul = num3 * num4 ;
-console.log(mul)
-
-console.log(typeof(reshma))
-console.log(typeof(num1))
-console.log(typeof(num2))
-
+console.log("sub :" +(num1-num2))
+console.log("add :" +(num1+num2))
+console.log("mul :" +(num1*num2))
+console.log("div :" +(num1/num2))
